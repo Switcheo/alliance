7e0907ab22fb6d1aa10c6c6f98c9ffc5dfeb40ce
Nicholas Chung <84615041+chungnicholas@users.noreply.github.com>
2023-07-19 16:46:43 +0800

    fix/remove alliance telemetry logs (#1)
    * Remove telemetry
    
    * Update abci.go

--- x/alliance/abci.go
+++ x/alliance/abci.go
@@ -4,16 +4,13 @@ import (
 	"fmt"
 
 	"github.com/terra-money/alliance/x/alliance/keeper"
-	"github.com/terra-money/alliance/x/alliance/types"
 
-	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
 // EndBlocker
 func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
-	defer telemetry.ModuleMeasureSince(types.ModuleName, ctx.BlockTime(), telemetry.MetricKeyEndBlocker)
 	k.CompleteRedelegations(ctx)
 	if err := k.CompleteUndelegations(ctx); err != nil {
 		panic(fmt.Errorf("failed to complete undelegations from x/alliance module: %s", err))
